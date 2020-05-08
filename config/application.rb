754d65c532f0f558d06b4832bac932fbab017b7a
Kino Kiaro <kiarokino@gmail.com>
2020-05-08 20:28:23 +0900

    execute rails g scaffold

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,12 @@ module ForTorLv3
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.generators do |g|
+      g.template_engine false
       g.javascripts false
       g.stylesheets false
       g.helper false
       g.test_framework false
     end
+    config.api_only = true
   end
 end
